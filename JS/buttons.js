7eb8563aba9adbcdbd1368eabc5733dc27f9a00f
alca8781 <123039964+alca8781@users.noreply.github.com>
2024-10-06 13:56:46 -0600

    galleries

--- JS/buttons.js
+++ JS/buttons.js
@@ -1,9 +1,16 @@
 
-
+// Instagram button 
 document.getElementById("instagram-button").addEventListener("click", ()=> {
     window.location.href = 'https://www.instagram.com/ally.cady.photography/'; 
 });
 
+
+// Book a shoot button
 document.getElementById("book").addEventListener("click", ()=> {
     window.location.href = 'contact.html'; 
+});
+
+// View Galleries 
+document.getElementById("findPhotosBtn").addEventListener("click", ()=> {
+    window.location.href = 'FindGal.html'; 
 });
--- JS/contact.js
+++ JS/contact.js
@@ -5,20 +5,11 @@ document.getElementById('FormInput').addEventListener('submit', function(event){
     const email = document.getElementById('email').value; 
     const message = document.getElementById('message').value; 
 
-    if (name && email && message) {
-        emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
-            from_name: name,
-            from_email: email,
-            to_name: 'Ally Cady',
-            message: message,
-            to_email: 'ac@allycadyphotos.com',
-        })
-        .then(function(response) {
-            document.getElementById('FormMessage').innerText = 'Thank you for reaching out! The photographer will get back to you as soon as possible!';
-        }, function(error) {
-            document.getElementById('FormMessage').innerText = 'There was an error sending the message. Please try again later.';
-        });
-    } else {
+    if(name && email && message){
+        document.getElementById('FormMessage').innerText = 'Thank you for reaching out! The photographer will get back to you as soon as possible!'; 
+        document.getElementById('FormMessage').reset(); 
+    }
+    else{
         document.getElementById('FormMessage').innerText = 'Please fill out all fields';
     }
-});
+})
