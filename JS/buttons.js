8e88386ac626ca743b762f54c8c97bc7a3816a0b
alca8781 <123039964+alca8781@users.noreply.github.com>
2024-10-05 23:27:13 -0600

    pages

---
+++ JS/buttons.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("instagram-button").addEventListener("click", ()=> {
+    window.location.href = 'https://www.instagram.com/ally.cady.photography/'; 
+});
+
+document.getElementById("book").addEventListener("click", ()=> {
+    window.location.href = 'contact.html'; 
+});
