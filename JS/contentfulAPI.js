c55f70290e3a7f399bc1695c2374d80cf31bcd24
alca8781 <123039964+alca8781@users.noreply.github.com>
2024-10-12 00:20:41 -0600

    small screen

--- JS/contentfulAPI.js
+++ JS/contentfulAPI.js
@@ -1,15 +1,31 @@
-fetch('https://github.com/contentful/contentful.js')
-
-
-
-const contentful = require('contentful')
-
+import * as contentful from "contentful"
 const client = contentful.createClient({
-  space: 'iz0llk80rvzb',
-  environment: 'master', // defaults to 'master' if not set
-  accessToken: 'd2HXMWGMqyvnW8eBK3Hgu4upxzzakCBi4qQv4p627Aw'
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: 'rlx5jqzvnc1n',
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: '5OGYgwN18wUBl178FBfIyZvq2jNAGAoVUnVjBE7JoZU',
 })
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token
+client.getSpace('rlx5jqzvnc1n')
+.then((space) => space.getEnvironment('PhotoGalleriesEnv'))
+  .then((environment) => {
+    "fields": {
+    "title": "Playsam Streamliner",
+    "file": {
+      "fileName": "quwowooybuqbl6ntboz3.jpg",
+      "contentType": "image/jpg",
+      "details": {
+        "image": {
+          "width": 600,
+          "height": 446
+        },
+        "size": 27187
+      },
+      "url": "//images.ctfassets.net/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg"
+    }
+  }
+  });
 
-client.getEntry('1dWMgZjiSc61M6ZVD49FrV')
-  .then((entry) => console.log(entry))
-  .catch(console.error) 
+
+
+
